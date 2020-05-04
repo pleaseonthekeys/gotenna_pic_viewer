a76214c6da0502df258b23c7631e1d6ab05077c1
Lauren Albert <pleaseonthekeys@gmail.com>
2020-05-03 23:19:48 -0400

    establishes cors policy

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const router = require('../routes');
 
 const app = express();
+
+const defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10,
+};
+
+app.use(cors(defaultCorsHeaders));
 app.use(morgan('dev'));
 //parse body
 app.use(bodyParser.json());
